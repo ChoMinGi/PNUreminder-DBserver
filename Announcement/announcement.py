8d9b78422b55077ab3798d0938025c3126b290a2
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-05-08 16:55:06 +0900

    Update announcement

--- Announcement/announcement.py
+++ Announcement/announcement.py
@@ -1,4 +1,4 @@
-from amazon_rds.rds_connection import create_rds_session
+from amazon_rds.rds_connection import create_rds_session_for_announcement
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
@@ -9,17 +9,17 @@ def save_culedu_announce_to_rds(announcements):
     class CuleduAnnounce(Base):
         __tablename__ = 'culedu_announce'
 
-        id = Column(Integer, primary_key=True)
+        id = Column(Integer, primary_key=True, autoincrement=True)
         title = Column(String)
-        content = Column(String)
+        urls = Column(String)
 
-    session = create_rds_session()
+    session = create_rds_session_for_announcement()
     # Create RDS session
 
     for announce in announcements:
         new_announce = CuleduAnnounce(
             title=announce['title'],
-            content=announce['content']
+            urls=announce['urls']
         )
         session.add(new_announce)
 
--- Announcement/crawlAnnouncement.py
+++ Announcement/crawlAnnouncement.py
@@ -4,11 +4,16 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-import mysql.connector
 
+from Announcement.announcement import save_culedu_announce_to_rds
 
+from gpt_summarize.main import summarize_text
 
+MAX_TOKENS = 3500 - 150  # 150 tokens for completion
+def truncate_content(content):
+    tokens = content.split()
+    truncated_tokens = tokens[:MAX_TOKENS]
+    return ' '.join(truncated_tokens)
 def crawlAnnouncement(driver):
     # Get HTML
     driver.get('https://culedu.pusan.ac.kr/culedu/15027/subview.do')
@@ -17,22 +22,39 @@ def crawlAnnouncement(driver):
 
     data = []
 
-    for idx, title_element in enumerate(titles_elements):
+    # Collect all URLs first
+    urls_list = []
+    for title_element in titles_elements:
         title = title_element.text
-        link = title_element.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        urls = title_element.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        urls_list.append({"title": title, "urls": urls})
+
+    # Visit each URL and perform summarization
+    for url_info in urls_list:
+        title = url_info['title']
+        urls = url_info['urls']
+
+        driver.get(urls)
+        driver.implicitly_wait(3)
+        content = driver.find_element(By.CLASS_NAME, "artclView").text
+
+
+
+        # Then, in the crawlAnnouncement function:
+        truncated_content = truncate_content(content)
+        for i in range(5):
+            summarize_text(title, truncated_content)
 
         driver.execute_script("window.open('');")
         driver.switch_to.window(driver.window_handles[-1])
 
-        driver.get(link)
-        driver.implicitly_wait(3)
-        content = driver.find_element(By.CLASS_NAME, "artclView").get_attribute('innerHTML')
-
-        data.append({"title": title, "content": content})
+        data.append({"title": title, "urls": urls})
 
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
         driver.implicitly_wait(3)
+
+        break
 
     driver.quit()
 
@@ -40,22 +62,7 @@ def crawlAnnouncement(driver):
 
     return data
 
-def save_to_database(data):
-    # Connect to the database
-    cnx = mysql.connector.connect(user='your_user', password='your_password', host='your_host', database='your_database')
-    cursor = cnx.cursor()
 
-    # Insert data into the database
-    for item in data:
-        title = item['title']
-        content = item['content']
-        insert_query = "INSERT INTO your_table (title, content) VALUES (%s, %s)"
-        cursor.execute(insert_query, (title, content))
-
-    # Commit the changes and close the connection
-    cnx.commit()
-    cursor.close()
-    cnx.close()
 
 if __name__ == "__main__":
     # Initialize the webdriver
@@ -65,7 +72,8 @@ if __name__ == "__main__":
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     # Crawl the data
-    crawled_data = crawlAnnouncement(driver)
+    # crawled_data = crawlAnnouncement(driver)
+    crawlAnnouncement(driver)
 
     # Save the data to the database
-    # save_to_database(crawled_data)
+    # save_culedu_announce_to_rds(crawled_data)
