8f1faf5089521dd9341f83a12624700144ac7225
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-05-08 17:23:34 +0900

    Update rds_connection.py

--- amazon_rds/rds_connection.py
+++ amazon_rds/rds_connection.py
@@ -4,17 +4,20 @@ import pymysql.cursors
 
 #RDS imformation
 from amazon_rds.db_info import dbsecurity
+from amazon_rds.db_info import dbsecurity_for_announcement
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 def create_rds_session(return_engine=None):
 
-    host = dbsecurity.host
+    dbsecurity_instance = dbsecurity()
+    host = dbsecurity_instance.host
     port = 3306
-    username = dbsecurity.username
-    password = dbsecurity.password
-    db_name = dbsecurity.database
+    username = dbsecurity_instance.username
+    password = dbsecurity_instance.password
+    db_name = dbsecurity_instance.database
 
     # RDS 연결 문자열 생성
     database_uri = f'mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}'
@@ -28,6 +31,27 @@ def create_rds_session(return_engine=None):
     else:
         return engine
 
+
+def create_rds_session_for_announcement(return_engine=None):
+
+    dbsecurity_instance = dbsecurity_for_announcement()
+    host = dbsecurity_instance.host
+    port = 3306
+    username = dbsecurity_instance.username
+    password = dbsecurity_instance.password
+    db_name = dbsecurity_instance.database
+
+    # RDS 연결 문자열 생성
+    database_uri = f'mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}'
+
+    # 엔진 생성 및 세션 설정
+    engine = create_engine(database_uri)
+    Session = sessionmaker(bind=engine)
+
+    if return_engine is None:
+        return Session()
+    else:
+        return engine
 
 def connect_rds_pymysql():
     try:
