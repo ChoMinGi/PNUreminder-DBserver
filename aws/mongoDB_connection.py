1a0b22f3a86794dee5149effff1516b0632e5b05
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-05-25 14:47:20 +0900

    Migration DB RDS -> MongoDB

--- aws/mongoDB_connection.py
+++ aws/mongoDB_connection.py
@@ -4,23 +4,7 @@ from aws.db_info import mongo_db
 
 def mongoDB_connection():
     client = MongoClient(f'mongodb://{mongo_db.USER_NAME}:{mongo_db.PASSWORD}@{mongo_db.HOST}:27017/')
-    db = client['test_db']
-
-    # 'test_collection'라는 이름의 컬렉션을 선택합니다. 이 컬렉션도 존재하지 않으면 새로 생성됩니다.
-    collection = db['test_collection']
-
-    # 컬렉션에 삽입할 간단한 데이터를 만듭니다.
-    data = {'name': '민준쓰', 'age': 25, 'job': 'no-job'}
-
-    # 데이터를 컬렉션에 삽입합니다.
-    insert_result = collection.insert_one(data)
-    print(f'Inserted data with id {insert_result.inserted_id}')
-
-    # 컬렉션에서 이름이 'John'인 데이터를 검색합니다.
-    query = {'name': 'John'}
-    query_result = collection.find_one(query)
-    print(query_result)
-    return
+    return client
 
 
 mongoDB_connection()
--- gpt_summarize/main.py
+++ gpt_summarize/main.py
@@ -1,16 +1,38 @@
 import openai
 
+from bs4 import BeautifulSoup
+
 # API 키 설정
+
+def get_each_html(driver,title, url):
+    driver.get(url)
+    driver.implicitly_wait(10)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    content = soup.find('div', {'class': 'artclView'}).get_text(strip=True)
+
+    # 본문 내용을 여러 부분으로 나눕니다.
+    # parts = split_text(content, MAX_LENGTH)
+    #
+    # # 각 부분을 요약합니다.
+    # for part in parts:
+    #     summarized_part = summarize_text(title, part)
+    #     print(summarized_part)
+    try:
+        summarized_part = summarize_text(title, content)
+        return summarized_part
+    except:
+        return
 
 
 def summarize_text(title,text):
-    openai.api_key = "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"
+    openai.api_key = "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"
     # 요약할 텍스트 입력
     prompt = f"제목 : {title} 내용:{text}\n\n 이 게시글을 사용자가 검색으로 접근하거나 이 게시글이 필요한 사용자에게 이 글을 설명할 수 있는 키워드를 5개에서 8개 사이로 보여줘 출력 형식은 키워드 사이에 , 를 추가해서 설명없이 출력하게 해주세요."
 
     # 요청을 위한 파라미터 설정
     params = {
-        "engine": "text-davinci-002",
+        "engine": "text-davinci-003",
         "prompt": prompt,
         "max_tokens": 200,
         "n": 5,
@@ -23,7 +45,5 @@ def summarize_text(title,text):
 
     # 요약 결과 추출 및 반환
     summary = response.choices[0].text.strip()
-    print(summary)
     return summary
 
-# 사용 예시
